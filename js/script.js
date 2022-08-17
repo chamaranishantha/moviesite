2b1ad8266dc970cb613013fd03f3bb507ff2abae
chamaranishantha <chamaranishantha9@gmail.com>
2022-08-17 22:18:10 +0530

    v1

--- js/script.js
+++ js/script.js
@@ -1,62 +1,68 @@
 jQuery("#carousel").owlCarousel({
   autoplay: true,
   rewind: false,
-  loop:true,
+  loop: true,
   margin: 20,
-  animateOut: 'fadeOut',
-  animateIn: 'fadeIn',
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
   responsiveClass: true,
   autoHeight: true,
   autoplayTimeout: 2000,
   smartSpeed: 800,
   nav: true,
-  navText: ["<img class='slider-nav-icon nav-left' src='img/nav-left-icon.png'></img>", "<img class='slider-nav-icon nav-right' src='img/nav-right-icon.png'></img>"],
+  navText: [
+    "<img class='slider-nav-icon nav-left' src='img/nav-left-icon.png'></img>",
+    "<img class='slider-nav-icon nav-right' src='img/nav-right-icon.png'></img>",
+  ],
   responsive: {
     0: {
-      items: 1
+      items: 1,
     },
 
     600: {
-      items: 3
+      items: 3,
     },
 
     1024: {
-      items: 3
+      items: 3,
     },
 
     1366: {
-      items: 3
-    }
-  }
+      items: 3,
+    },
+  },
 });
 jQuery("#carousel2").owlCarousel({
   autoplay: true,
   rewind: false,
-  loop:true,
+  loop: true,
   margin: 20,
-  animateOut: 'fadeOut',
-  animateIn: 'fadeIn',
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
   responsiveClass: true,
   autoHeight: true,
   autoplayTimeout: 2000,
   smartSpeed: 800,
   nav: true,
-  navText: ["<img class='slider-nav-icon nav-left' src='img/nav-left-icon.png'></img>", "<img class='slider-nav-icon nav-right' src='img/nav-right-icon.png'></img>"],
+  navText: [
+    "<img class='slider-nav-icon nav-left' src='img/nav-left-icon.png'></img>",
+    "<img class='slider-nav-icon nav-right' src='img/nav-right-icon.png'></img>",
+  ],
   responsive: {
     0: {
-      items: 1
+      items: 1,
     },
 
     600: {
-      items: 3
+      items: 3,
     },
 
     1024: {
-      items: 3
+      items: 3,
     },
 
     1366: {
-      items: 3
-    }
-  }
+      items: 3,
+    },
+  },
 });
